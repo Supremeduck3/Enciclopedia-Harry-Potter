3df03e6351d4c5152390404fa91acedcf5aa43b1
emiliott <favorettoemilip@gmail.com>
2026-08-10 11:38:51 -0300

    pagina sobre

--- src/app/Sobre/page.jsx
+++ src/app/Sobre/page.jsx
@@ -1,30 +1,32 @@
 import React from 'react';
 import styles from './sobre.module.css';
-import Image from 'next/image';
 
 export default function sobre() {
     return (
         <main className={styles.main}>
-            <Image className={styles.main} />
+            <div className={styles.hero}>
             <div className={styles.container}>
                 <div className={styles.card}>
-                    <h2 className={styles.turma}>Turma:</h2>
-                    <p>2TDS1</p>
+                    <h1 className={styles.turma}>Turma:</h1>
+                    <h1 className={styles.tds}>2TDS1</h1>
                 </div>
                 <div className={styles.card}>
-                    <h2 className={styles.t_prof}>Professores</h2>
-                    <p className={styles.prof}>Marcelo Carboni</p>
-                    <p className={styles.prof}>Thiago Ferreira</p>
+                    <h1 className={styles.t_prof}>Professores</h1>
+                    <h3 className={styles.prof}>Marcelo Carboni</h3>
+                    <h3 className={styles.prof}>Thiago Ferreira</h3>
                 </div>
                 <div className={styles.card}>
-                    <h2 className={styles.t_curso}>Curso</h2>
-                    <p className={styles.curso}>Desenvolvimento de sistemas</p>
+                    <h1 className={styles.t_curso}>Curso</h1>
+                    <h3 className={styles.curso}>Desenvolvimento de sistemas</h3>
                 </div>
                 <div className={styles.card}>
                     <h2 className={styles.t_lista}>Lista</h2>
-                    <p className={styles.lista}>Emílio Henrique dos Santos Favoretto Pedro Otávio de Andrade Braga</p>
+                    <p className={styles.lista}>
+                        Emílio Henrique dos Santos Favoretto Pedro Otávio de Andrade Braga
+                    </p>
                     <p>Pedro Otávio de Andrade Braga</p>
                 </div>
+            </div>
             </div>
         </main>
     );
