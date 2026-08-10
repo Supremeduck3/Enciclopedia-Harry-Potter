a021228ab146d2ea2debf7892d2675b97e8546d1
supremeduck008 <pedro.otaviobraga@gmail.com>
2026-08-10 11:42:23 -0300

    continua a home

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,17 +4,27 @@ import Image from 'next/image';
 export default function Home() {
     return (
         <>
-        <Header/>
-        <Image
-        src={"/images/banner3.jpg"}
-        width={1000}
-        height={500}
-        alt='Banner Harry potter'
-        className={styles.imagem}
-        />
-        <main>
-            
-        </main>
+            <Header />
+            <Image
+                src={"/images/banner4.webp"}
+                width={1000}
+                height={500}
+                alt='Banner Harry potter'
+                className={styles.imagem}
+            />
+            <main>
+                <section className={styles.grid}>
+                    <div className={styles.sobre}>
+                        <h1>Sobre</h1>
+                    </div>
+                    <div className={styles.tecnologias}>
+                        <h1>Tecnologias</h1>
+                    </div>
+                    <div className={styles.objetivo}>
+                        <h1>Objetivo</h1>
+                    </div>
+                </section>
+            </main>
         </>
     );
 }
