4d3363eac519f4d7d9c13126d7daee926ba81984
supremeduck008 <pedro.otaviobraga@gmail.com>
2026-08-10 11:01:44 -0300

    começaa home

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,20 @@
 import styles from './page.module.css';
 import Header from './../components/Header';
-
+import Image from 'next/image';
 export default function Home() {
     return (
         <>
         <Header/>
+        <Image
+        src={"/images/banner3.jpg"}
+        width={1000}
+        height={500}
+        alt='Banner Harry potter'
+        className={styles.imagem}
+        />
+        <main>
+            
+        </main>
         </>
     );
 }
